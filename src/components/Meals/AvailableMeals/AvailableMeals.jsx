5bba9d60e4e4a08c6aded739551f0646e9dab373
Fahmi <ghokemons@gmail.com>
2023-10-02 23:03:04 +0700

    fix: fetch available meals from database

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,49 +1,56 @@
+import { useEffect, useState } from "react";
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealList/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    const fetchRequest = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://react-meals-10119-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+        );
+        const responseData = await response.json();
+        setMeals(responseData);
+      } catch (err) {
+        setErrorMessage(err.message);
+      }
+
+      setIsLoading(false);
+    };
+    fetchRequest();
+  }, []);
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {DUMMY_MEALS.map((data) => (
-            <MealItem
-              id={data.id}
-              key={data.id}
-              name={data.name}
-              description={data.description}
-              price={data.price}
-            />
-          ))}
-        </ul>
+        {isLoading ? (
+          <h3 className={classes["text-center"]}>Loading...</h3>
+        ) : errorMessage ? (
+          <h3 className={`${classes["text-center"]} ${classes["text-red"]}`}>
+            {errorMessage}.
+          </h3>
+        ) : meals.length === 0 ? (
+          <h2 className={classes["text-center"]}>No Meals Available!</h2>
+        ) : (
+          <ul>
+            {meals.map((data) => (
+              <MealItem
+                id={data.id}
+                key={data.id}
+                name={data.name}
+                description={data.description}
+                price={data.price}
+              />
+            ))}
+          </ul>
+        )}
       </Card>
     </section>
   );
